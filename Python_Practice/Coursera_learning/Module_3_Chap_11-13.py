4cac2efaebb62c2799193d2bce19fdb552b0ac1f
Kokulakrishnan <kokulakrishnan@outlook.in>
2020-05-07 14:17:51 +0530

    update upto module 4.1 assignment.

--- Python_Practice/Coursera_learning/Module_3_Chap_11-13.py
+++ Python_Practice/Coursera_learning/Module_3_Chap_11-13.py
@@ -225,9 +225,9 @@ fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
     print(line.decode().strip())
 '''
-
-#handling webpage like a file 
 '''
+#handling webpage like a file 
+
 import urllib.request, urllib.parse, urllib.error
 
 fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
