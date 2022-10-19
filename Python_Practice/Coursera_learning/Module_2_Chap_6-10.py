f8f13f6d5e023af74b63061e29f9819382f3ef1d
Kokulakrishnan <kokulakrishnan@outlook.in>
2022-10-18 20:46:29 -0400

    new changes while preparing for walmart

--- Python_Practice/Coursera_learning/Module_2_Chap_6-10.py
+++ Python_Practice/Coursera_learning/Module_2_Chap_6-10.py
@@ -259,16 +259,16 @@ print(ccc['csev])
 """
 
 #when we see a new name
-""" 
+
 counts = dict()
 names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
 for name in names:
     if name not in counts:
         counts[name] = 1
     else:
-        counts[name] = counts[name] + 1
+        counts[name] = counts.get(name, 0)+1
 print(counts)
-"""
+[9,41,12,3,74,15]
 
 #the get method for dictionaries 
 """
